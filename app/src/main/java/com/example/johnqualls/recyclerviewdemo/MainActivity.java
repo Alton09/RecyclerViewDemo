3a69d30243dd36b77e3d2ea3a5e013d8eab8c029
John Qualls <altonqualls@gmail.com>
2017-03-13 08:48:24 -0600

    RecyclerView Implemented with LinearLayout.

--- app/src/main/java/com/example/johnqualls/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/johnqualls/recyclerviewdemo/MainActivity.java
@@ -6,15 +6,17 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends Activity implements Button.OnClickListener{
     private static int count = 1;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private ArrayList<String> dataset;
+    private List<String> dataset;
     private Button addItem,
                    removeItem;
 
@@ -25,7 +27,10 @@ public class MainActivity extends Activity implements Button.OnClickListener{
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // Register buttons
-
+        addItem = (Button) findViewById(R.id.addItem);
+        removeItem = (Button) findViewById(R.id.removeItem);
+        addItem.setOnClickListener(this);
+        removeItem.setOnClickListener(this);
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -43,19 +48,27 @@ public class MainActivity extends Activity implements Button.OnClickListener{
         dataset.add("Jim");
 
         // specify an adapter (see also next example)
-        mAdapter = new TestAdapter((String[])dataset.toArray());
+        mAdapter = new TestAdapter(dataset);
         mRecyclerView.setAdapter(mAdapter);
     }
 
 
     @Override
     public void onClick(View view) {
-        if(view == findViewById(R.id.addItem)) {
+        if(view.getId() == R.id.addItem) {
             dataset.add("Event" + count);
             dataset.add("Name" + count);
+            mAdapter.notifyDataSetChanged();
+            count++;
+            Toast.makeText(this, "Add Item!", Toast.LENGTH_SHORT).show();
         }
         else {
-            dataset.remove(dataset.size() - 1);
+            int position = dataset.size() - 1;
+            dataset.remove(position);
+            mRecyclerView.removeViewAt(position);
+            mAdapter.notifyItemRemoved(position);
+            mAdapter.notifyItemRangeChanged(position, dataset.size());
+            Toast.makeText(this, "Remove Item!", Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/johnqualls/recyclerviewdemo/TestAdapter.java
+++ app/src/main/java/com/example/johnqualls/recyclerviewdemo/TestAdapter.java
@@ -3,30 +3,33 @@ package com.example.johnqualls.recyclerviewdemo;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by john.qualls on 3/12/2017.
  */
 
 public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
-    private String[] mDataset;
+    private List<String> mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView mTextView;
+        public LinearLayout mLinearLayout;
 
-        public ViewHolder(TextView v) {
+        public ViewHolder(LinearLayout v) {
             super(v);
-            mTextView = v;
+            mLinearLayout = v;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public TestAdapter(String[] myDataset) {
+    public TestAdapter(List<String> myDataset) {
         mDataset = myDataset;
     }
 
@@ -34,7 +37,7 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
     @Override
     public TestAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.row_view, parent, false);
         // set the view's size, margins, paddings and layout parameters
         //...
@@ -47,13 +50,15 @@ public class TestAdapter extends RecyclerView.Adapter<TestAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
-
+        if(position > 0 && position < mDataset.size() - 1) {
+            ((TextView) holder.mLinearLayout.getChildAt(0)).setText(mDataset.get(position));
+            ((TextView) holder.mLinearLayout.getChildAt(1)).setText(mDataset.get(position + 1));
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
     }
 }
